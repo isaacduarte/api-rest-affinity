51b400ffedc0b506a25ab30f161506017cefbf3d
isaacduarte <isaacramonc@gmail.com>
2019-10-02 18:00:17 -0300

    começo da migração para api rest

---
+++ conexao/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* desenvolvimento*/
+define('USUARIO', 'root');
+define('SENHA', '');
+define('ENDERECO', 'localhost');
+define('BANCO', 'affinity');
+define('CHARSET', 'utf8');
+
+/*produção
+define('USUARIO', 'arruda72_aff');
+define('SENHA', 'qaz123');
+define('ENDERECO', 'localhost');
+define('BANCO', 'arruda72_aff');
+define('CHARSET', 'utf8');
+ */
